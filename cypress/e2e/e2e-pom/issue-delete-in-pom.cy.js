1c04163bdcffde3cac663b413b44aa746dc6de7f
Andres-Ra <Andres.Raev@gmail.com>
2023-07-09 11:11:53 +0300

    Delete in POM functionality added

--- cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
+++ cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
@@ -3,23 +3,42 @@
  */
 import IssueModal from "../../pages/IssueModal";
 
-describe('Issue delete', () => {
-  beforeEach(() => {
-    cy.visit('/');
-    cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
-    //open issue detail modal with title from line 16  
-    cy.contains(issueTitle).click();
-    });
-  });
-
-  //issue title, that we are testing with, saved into variable
+describe('Tests for covering issue deletion functionality with POM approach', () => {
+  // Descrbing constant issueTitle to simplify the code      
   const issueTitle = 'This is an issue of type: Task.';
+  const issueDetails = '[data-testid="modal:issue-details"]';
+beforeEach(() => {
+  cy.visit('/');
+  cy.url().should('eq', `${Cypress.env('baseUrl')}project`).then((url) => {
+      cy.visit(url + '/board');
+      //Borard contains Task issue
+      cy.contains(issueTitle).click();
+      //Asserting that issue detail view modal is visible after clicking
+      cy.get(issueDetails).should('be.visible');
+      });
+  })
 
   it('Should delete issue successfully', () => {
-    //add steps to delete issue
-  });
+    // Clicking Delete button with POM approach
+    IssueModal.clickDeleteButton();
+
+    // Confirming Deletion with POM approach
+    IssueModal.confirmDeletion();
+
+    // Ensuring issueTitle is not visible on board with POM approach
+    IssueModal.ensureIssueIsNotVisibleOnBoard(issueTitle);
+    });
 
   it('Should cancel deletion process successfully', () => {
-    //add steps to start deletion proces but cancel it
+    IssueModal.clickDeleteButton();
+
+    // Canceling deletion with POM approach
+    IssueModal.cancelDeletion();
+
+    // Closing detail modal with POM approach
+    IssueModal.closeDetailModal();
+
+    // Ensuring Issue (with issueTitle) is visible on Board 
+    IssueModal.ensureIssueIsVisibleOnBoard(issueTitle);
   });
 });
